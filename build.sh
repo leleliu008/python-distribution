a119ee9e389d7dfa6f5da1c52358222a92481bac
leleliu008 <leleliu008@gmail.com>
2025-02-20 16:39:00 +0800

    optimized

--- build.sh
+++ build.sh
@@ -543,9 +543,9 @@ inspect_install_arguments() {
 
     #########################################################################################
 
-    PP_ARGS="$PP_ARGS -I$AUX_INCLUDE_DIR"
+    PP_ARGS="-I$AUX_INCLUDE_DIR $PP_ARGS"
 
-    LD_ARGS="$LD_ARGS -L$AUX_LIBRARY_DIR"
+    LD_ARGS="-L$AUX_LIBRARY_DIR $LD_ARGS"
 
     PATH="$AUX_INSTALL_DIR/bin:$PATH"
 
